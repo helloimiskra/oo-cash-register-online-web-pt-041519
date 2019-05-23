f0135bd04cd1e86aecfe769df5da3ca9b71a7a77
Iskra B <ibatistapoblete@gmail.com>
2019-05-23 03:20:07 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,13 +16,13 @@ class CashRegister
   end
 
   def apply_discount
+    if self.discount.empty?
+      return "There is no discount to apply."
+    else
       discount_percentage = @discount * 0.01
       @total = @total - (@total.to_f * discount_percentage)
       "After the discount, the total comes to $#{@total.to_i}."
-    else
-      "There is no discount to apply."
     end
-
   end
 
 end
