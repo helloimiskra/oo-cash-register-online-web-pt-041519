50cd579263e00ac34b92ae147d8a142df8fa485f
Iskra B <ibatistapoblete@gmail.com>
2019-05-23 03:03:35 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,7 @@ class CashRegister
   end
 
   def apply_discount
-    if self.include?(discount)
+    if include?(discount)
       discount_percentage = @discount * 0.01
       @total = @total - (@total.to_f * discount_percentage)
       "After the discount, the total comes to $#{@total.to_i}."
