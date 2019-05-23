ff2cff4395beae41eafeb480a99fb74b0efdc79e
Iskra B <ibatistapoblete@gmail.com>
2019-05-23 05:21:28 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
 
   def add_item(title, price = 0.0, quantity = 1)
     @total += price * quantity
-    @items << title
+    @item = @item.concat(((title + " ") * quantity).split(" "))
   end
 
   def apply_discount
